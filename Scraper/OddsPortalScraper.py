d342ae23417cd72fad782d82a6ba0529be49e507
Riley Cullen <cullenriley@seattleu.edu>
2021-10-21 17:12:55 -0700

    Chrome webdriver added

--- Scraper/OddsPortalScraper.py
+++ Scraper/OddsPortalScraper.py
@@ -3,7 +3,11 @@
 # Last updated 10/21/21
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 class OddsPortalScraper:
     def __init__(self):
-        pass
+        options = Options()
+        options.headless = True
+        self.__webdriver = webdriver.Chrome('Users/rileycullen/chromedriver', 
+            options=options)
