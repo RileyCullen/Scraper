373267e5622e0d4318f88714ad87d5e4939522c7
Riley Cullen <cullenriley@seattleu.edu>
2021-10-22 16:44:16 -0700

    ParseTennis function added

--- Shell.py
+++ Shell.py
@@ -12,7 +12,9 @@ def main():
         tokens = request.split(' ')
 
         if (tokens[0] == 'scrape'):
-            pass
+            # if only 'scrape' is typed, then start from /tennis/ and scrape down
+            if (len(tokens) == 1):
+                ParseTennis()
         elif (tokens[0] == 'login'):
             if (len(tokens) == 3):
                 status = scraper.Login(tokens[1], tokens[2])
@@ -26,6 +28,11 @@ def main():
 # desc: Print an error message     
 def PrintError(message):
     print('Error: ' + message)
-    
+
+# desc: Start at /tennis/ page, find all of the tournaments, then find all of
+# the tournament matches and scrape that for betmaker odds
+def ParseTennis():
+    pass
+
 if __name__ == '__main__':
     main()
