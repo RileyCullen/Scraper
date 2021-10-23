c29e2a8993b3fbecc4a88616ae730d160742dd7e
Riley Cullen <cullenriley@seattleu.edu>
2021-10-23 16:43:12 -0700

    SetData() added

--- View.py
+++ View.py
@@ -6,3 +6,12 @@ class ViewJSON:
     # desc: Constructor for ViewJSON class
     def __init__(self):
         self._data = 0
+
+    # desc: Updates _data
+    # 
+    # Parameters:
+    # ---------------
+    # data : JSON object
+    #   A JSON object consisting of data obtained from the OddsPortal
+    def SetData(self, data):
+        self._data = data
