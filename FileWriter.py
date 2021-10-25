d445ab38b96781a6d3beff98bf6fee9501ee5930
Riley Cullen <cullenriley@seattleu.edu>
2021-10-25 13:15:26 -0700

    Initial commit

---
+++ FileWriter.py
@@ -0,0 +1,10 @@
+# desc: Takes in a JSON object and writes it to a file
+#
+# Parameters:
+# ---------------
+# data : JSON
+#   The JSON object you want to write to a file
+# filename : string
+#   The name of the file
+def WriteToJSON(data, filename = 'data.json'):
+    pass
