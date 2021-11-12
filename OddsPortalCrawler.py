a71cda62336ce535a965689df70c2cae8d04dc2d
Riley Cullen <cullenriley@seattleu.edu>
2021-11-12 10:29:31 -0800

    Documentation added'

--- OddsPortalCrawler.py
+++ OddsPortalCrawler.py
@@ -5,48 +5,63 @@ from FileReader import ReadFromJSON
 from Scraper.OddsPortalScraper import OddsPortalScraper
 import datetime, sys, threading, time, copy
 
+# desc: Defines entry point into the program.
 def Main():
+    # Don't proceed with the program unless the user gives a username and 
+    # password.
     if len(sys.argv) < 2:
         print('Please enter username and password')
         return
 
+    # Create a scraper object to login and obtain tournament information
     scraper = OddsPortalScraper(headless=True)
     scraper.Login(sys.argv[1], sys.argv[2])
     Crawl(scraper, sys.argv[1], sys.argv[2])
 
+# desc: Defines crawling logic.
 def Crawl(scraper, username, password):
     startTime = time.time()
 
+    # Get user input on if we want to read from an existing file or not.
     readFromFile = input('Read from existing file (Y/N)? ')
     filename = ''
 
     data = {}
     if (readFromFile == 'Y' or readFromFile == 'y'):
+        # If yes, get user input on which filename we should read from.
         filename = input('Read from: ')
         data = ReadFromJSON(filename)
     else:
+        # If no, define the new filename you want to create
         filename = input('Data output filename: ')
 
+    # Get input on how many iterations the scraper should run for.
     scrapeCount = int(input('Scrape iterations (enter -1 for infinite): '))
 
+    # Actual crawling code
     print('Scraping... (note this may take a while)')
     while (scrapeCount > 0):
         data = ParseTennis(scraper, username, password, data)
-        probabilityData = CalculateProbability(data)
         WriteToJSON(data, filename)
         print('Elapsed: ' + str(time.time() - startTime))
         scrapeCount -= 1
 
+# Desc: Defines how to collect and structure the odds data.
 def ParseTennis(scraper, username, password, data = {}):
     time.sleep(3)
+    # Get all the tournaments and remove doubles matches.
     tournaments = RemoveTournaments(scraper.GetTournaments('https://www.oddsportal.com/tennis/'), ['Doubles'])
     threads = []
 
     for tournament in tournaments:
+        # For each tournament, create an OddsPortalScraper, login, then create
+        # a new thread object for the tournament scraper.
         tournamentScraper = OddsPortalScraper(headless=True)
         tournamentScraper.Login(username, password)
         dataCopy = copy.deepcopy(data)
 
+        # Pass the current tournament link, the current scraper, and a copy of 
+        # the existing data to the thread. Start the thread.
         th = TournamentParser(target=ParseTournament, args=(tournament, tournamentScraper\
             , dataCopy))
         th.start()
@@ -55,27 +70,37 @@ def ParseTennis(scraper, username, password, data = {}):
     tmp = {}
     tArr = []
 
+    # Wait for all the threads to finish. On finish, each thread will return
+    # the tournament data (matches, timestamps, odds, etc).
     for th in threads:
         tArr.append(th.join())
 
+    # Combine data into one dictionary object.
     print('Combining data...')
+    # Run loop for all the data returned from each thread (tArr).
     for tData in tArr:
+        # For each key in tArr (note this should only be 1)
         for key in tData:
+            # Create a new tournament entry in tmp if the key does not exist.
             if (key not in tmp.keys()):
                 tmp[key] = {}
+            # For each match in the tournament
             for match in tData[key]:
+                # Create a new match entry in tmp if the key does not exist
                 if (match not in tmp[key].keys()):
                     tmp[key][match] = {}
+                # For each timestamp in the match data
                 for timestamp in tData[key][match]:
+                    # Create a new timestamp if the key does not exist.
                     if (timestamp not in tmp[key][match]):
                         tmp[key][match][timestamp] = tData[key][match][timestamp]
 
     return tmp
 
+# desc: Defines scraping logic. NOTE that this is the function that each thread
+# will be running.
 def ParseTournament(tournament, scraper, data):
-    # print('\n**********Tournament: ' + tournament['name'])
     matches = scraper.GetMatches(tournament['link'])
-    # print(matches)
     tournamentKey = tournament['name'].replace(' ', '-')
     hasTournamentKey = True
     # if the tournament entry does NOT exist, create one
@@ -99,6 +124,8 @@ def ParseTournament(tournament, scraper, data):
         data[tournamentKey][matchKey][timestamp] = odds 
     return data
 
+# I am too lazy to properly document this class, but essentially, we need to 
+# create this so that each thread has a return value.
 class TournamentParser(threading.Thread):
     def __init__(self, group=None, target=None, name=None, args=(), kwargs={}, Verbose=None):
         threading.Thread.__init__(self, group, target, name, args, kwargs)
